37f0f5978a552e9b9187e9822237820353385099
Nikhil Shanbhogue <81545183+MSN3@users.noreply.github.com>
2026-09-11 00:33:48 -0400

    add path template

--- baseline_code/config.py
+++ baseline_code/config.py
@@ -2,25 +2,20 @@
 
 # ----------------- MODEL CONFIGURATION ----------------- #
 # The model used for generation (e.g., Qwen, MedGemma)
-MODEL_NAME = "google/medgemma-27b-text-it"
-BASE_URL = "http://172.27.150.124:8000/v1" # URL for the agent model
+MODEL_NAME = "Medgemma"
+BASE_URL = "http://..." # URL for the agent model
 
 # The separate, more powerful model used for evaluation
-JUDGE_MODEL_NAME = "google/medgemma-27b-text-it" 
-JUDGE_BASE_URL = "http://172.27.150.124:8000/v1"
-
-# SIMILARITY_MODEL_NAME = 'Qwen/Qwen3-Embedding-4B'
-# # The cosine similarity score above which two diagnoses are considered equivalent.
-# # Typically, a value between 0.75 and 0.9 is a good starting point.
-# SIMILARITY_THRESHOLD = 0.8
+JUDGE_MODEL_NAME = "Qwen" 
+JUDGE_BASE_URL = "http://..."
 
 # ----------------- PROCESSING CONFIGURATION ----------------- #
 # Number of patient records to process in each concurrent batch.
 CHUNK_SIZE = 100
 
 # ----------------- FILE PATHS ----------------- #
-PATIENTS_FILE_PATH = '/storage/home/hcoda1/5/nshanbhogue6/scratch/frugal/data/unified_full_mimic.csv'
-TEST_LABELS_FILE_PATH = '/storage/home/hcoda1/5/nshanbhogue6/scratch/frugal/data/40_cases_unique_test_codes_with_labels_with_costs.csv'
+PATIENTS_FILE_PATH = '...'
+TEST_LABELS_FILE_PATH = '...'
 
 # ----------------- MEDICAL KNOWLEDGE ----------------- #
 # (This section remains unchanged)
